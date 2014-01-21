58a0ccb88175d9d6c5dd10501eab03e3a6d27fba
Dahua Lin <lindahua@gmail.com>
2014-01-21 13:44:53 -0600

    add setindex! methods

--- src/generic.jl
+++ src/generic.jl
@@ -63,3 +63,37 @@ getindex(a::ArrayView, i1::Int, i2::Int, i3::Int, i4::Int, i5::Int, i6::Int, I::
 
 # setindex
 
+setindex!(a::ArrayView, v, i::Real) = setindex!(a, v, to_index(i))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real) = setindex!(a, v, to_index(i1), to_index(i2))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real, i3::Real) = 
+    setindex!(a, v, to_index(i1), to_index(i2), to_index(i3))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real, i3::Real, i4::Real) = 
+    setindex!(a, v, to_index(i1), to_index(i2), to_index(i3), to_index(i4))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real, i3::Real, i4::Real, i5::Real) = 
+    setindex!(a, v, to_index(i1), to_index(i2), to_index(i3), to_index(i4), to_index(i5))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real, i3::Real, i4::Real, i5::Real, i6::Real) = 
+    setindex!(a, v, to_index(i1), to_index(i2), to_index(i3), to_index(i4), to_index(i5), to_index(i6))
+
+setindex!(a::ArrayView, v, i1::Real, i2::Real, i3::Real, i4::Real, i5::Real, i6::Real, I::Int...) = 
+    setindex!(a, v, to_index(i1), to_index(i2), to_index(i3), to_index(i4), to_index(i5), to_index(i6), I...)
+
+
+setindex!{T}(a::ArrayView{T}, v, i::Int) = arrayset(a.arr, convert(T, v), uindex(a, i))
+setindex!{T}(a::ArrayView{T}, v, i1::Int, i2::Int) = arrayset(a.arr, convert(T, v), uindex(a, i1, i2))
+setindex!{T}(a::ArrayView{T}, v, i1::Int, i2::Int, i3::Int) = arrayset(a.arr, convert(T, v), uindex(a, i1, i2, i3))
+
+setindex!{T}(a::ArrayView{T}, v, i1::Int, i2::Int, i3::Int, i4::Int) = 
+	arrayset(a.arr, convert(T, v), uindex(a, i1, i2, i3, i4))
+
+setindex!{T}(a::ArrayView{T}, v, i1::Int, i2::Int, i3::Int, i4::Int, i5::Int) = 
+	arrayset(a.arr, convert(T, v), uindex(a, i1, i2, i3, i4, i5))
+	
+setindex!{T}(a::ArrayView{T}, v, i1::Int, i2::Int, i3::Int, i4::Int, i5::Int, i6::Int, I::Int...) = 
+    arrayset(a.arr, convert(T, v), uindex(a, i1, i2, i3, i4, i5, i6, I...))
+
+
