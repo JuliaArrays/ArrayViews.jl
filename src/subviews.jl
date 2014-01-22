6dad6eecac430b9bf0958d5500768639c1d1cee7
Dahua Lin <lindahua@gmail.com>
2014-01-22 16:43:33 -0600

    subviews tested on a small number of simple cases

--- src/subviews.jl
+++ src/subviews.jl
@@ -1,3 +1,55 @@
 # subviews of a dense array
 
+typealias Indexer Union(Real,Range1,Range)
+
+#### compute view offset
+
+_offset(i::Colon) = 0
+_offset(i::Int) = i - 1
+_offset(i::Real) = to_index(i) - 1
+_offset(i::Ranges) = to_index(first(i)) - 1
+
+voffset(a::Array, i::Colon) = 0
+voffset(a::Array, i::Indexer) = _offset(i)
+
+voffset(a::Array, i1::Colon, i2::Colon) = 0
+voffset(a::Array, i1::Colon, i2::Indexer) = size(a,1) * _offset(i2)
+voffset(a::Array, i1::Indexer, i2::Colon) = _offset(i1)
+voffset(a::Array, i1::Indexer, i2::Indexer) = _offset(i1) + size(a,1) * _offset(i2)
+
+#### compute view shape
+
+# 1D view
+
+vshape(a::DenseArray, i::Colon) = (length(a),)
+vshape(a::DenseArray, i::Ranges) = (length(i),)
+
+# 2D view
+
+_succlen2{T}(a::DenseArray{T,1}) = 1
+_succlen2{T}(a::DenseArray{T,2}) = size(a, 2)
+_succlen2{T}(a::DenseArray{T,3}) = size(a, 2) * size(a, 3)
+_succlen2(a::DenseArray) = prod(size(a)[2:end])::Int
+
+vshape(a::DenseArray, i1::Colon, i2::Real) = (size(a,1),)
+vshape(a::DenseArray, i1::Ranges, i2::Real) = (length(i1),)
+
+vshape(a::DenseArray, i1::Real, i2::Colon) = (1, _succlen2(a))
+vshape(a::DenseArray, i1::Colon, i2::Colon) = (size(a,1), _succlen2(a))
+vshape(a::DenseArray, i1::Ranges, i2::Colon) = (length(i1), _succlen2(a))
+
+vshape(a::DenseArray, i1::Real, i2::Ranges) = (1, length(i2))
+vshape(a::DenseArray, i1::Colon, i2::Ranges) = (size(a,1), length(i2))
+vshape(a::DenseArray, i1::Ranges, i2::Ranges) = (length(i1), length(i2))
+
+
+#### views from arrays
+
+view(a::Array, i::Colon) = contiguous_view(a, voffset(a,i), vshape(a,i))
+view(a::Array, i::Range1) = contiguous_view(a, voffset(a,i), vshape(a,i))
+
+view(a::Array, i1::Colon, i2::Real) = contiguous_view(a, voffset(a, i1, i2), vshape(a, i1, i2))
+view(a::Array, i1::Range1, i2::Real) = contiguous_view(a, voffset(a, i1, i2), vshape(a, i1, i2))
+view(a::Array, i1::Colon, i2::Colon) = contiguous_view(a, voffset(a, i1, i2), vshape(a, i1, i2))
+view(a::Array, i1::Colon, i2::Range1) = contiguous_view(a, voffset(a, i1, i2), vshape(a, i1, i2))
 
---
+++ test/subviews.jl
@@ -0,0 +1,46 @@
+# testing subviews
+
+using ArrayViews
+
+function _test_view(a, r, subs...)
+	v = view(a, subs...)
+
+	siz_r = size(r)
+	siz_v = size(v)
+
+	if siz_r != siz_v
+		error("Incorrect size: get $(siz_v), but expect $(siz_r)")
+	end
+
+	for i = 1 : length(v)
+		if v[i] != r[i]			
+			println("v = ")
+			println(v)
+			println("r = ")
+			println(r)
+			error("Incorrect content.")
+		end
+	end
+end
+
+macro test_view(a_, subs...)
+	esc(:(_test_view($a_, ($a)[$(subs...)], $(subs...))))
+end
+
+
+
+#### test views from arrays
+
+const a = reshape(1.:1680., (8, 7, 6, 5))
+
+# 1D
+
+@test_view(a, :)
+@test_view(a, 1:12)
+
+# 2D
+
+@test_view(a, :, :)
+@test_view(a, :, 2)
+@test_view(a, :, 2:8)
+@test_view(a, 1:6, 2)
