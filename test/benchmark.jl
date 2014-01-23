5bb46822d396c9118b534c8856dce0da2db5e41b
Dahua Lin <lindahua@gmail.com>
2014-01-23 08:30:43 -0600

    benchmark fixed

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -2,76 +2,63 @@
 
 using ArrayViews
 
-function time_view1d(s::SubArray, v::ArrayView, rtimes::Int)
+mps(a::AbstractArray, rtimes::Int, et::Float64) = (length(a) * 1.0e-6) * rtimes / et
+
+function time_view1d(a::AbstractArray, rtimes::Int)
     # warm up
-    n = length(v)
-    @assert n == length(s)
-
+    n::Int = length(a)
     for i = 1:n
-        @inbounds s[i]
-    end
-    for i = 1:n
-        @inbounds v[i]
+        @inbounds a[i]
     end
 
-    et_s = @elapsed for t = 1 : rtimes
-        for i = 1:n 
-            @inbounds s[i]
-        end
-    end
-
-    et_v = @elapsed for t = 1 : rtimes
+    # benchmark
+    et = @elapsed for t = 1 : rtimes
         for i = 1:n
-            @inbounds v[i]
+            @inbounds a[i]
         end
     end
-
-    mps_s = (n / 1.0e6) * rtimes / et_s
-    mps_v = (n / 1.0e6) * rtimes / et_v
-
-    return (mps_s, mps_v)
+    return et
 end
 
-function time_view2d(s::SubArray, v::ArrayView, rtimes::Int)
+function time_view2d(a::AbstractArray, rtimes::Int)
     # warm up
-    m = size(v, 1)
-    n = size(v, 2)
-    @assert size(s) == (m, n)
-
+    m::Int = size(a, 1)
+    n::Int = size(a, 2)
     for j = 1:n, i = 1:m
-        @inbounds s[i,j]
-    end
-    for j = 1:n, i = 1:m
-        @inbounds v[i,j]
+        @inbounds a[i,j]
     end
 
-    et_s = @elapsed for t = 1 : rtimes
+    # benchmark
+    et = @elapsed for t = 1 : rtimes
         for j = 1:n, i = 1:m
-            @inbounds s[i,j]
+            @inbounds a[i,j]
         end
     end
-
-    et_v = @elapsed for t = 1 : rtimes
-        for j = 1:n, i = 1:m
-            @inbounds v[i,j]
-        end
-    end
-
-    mps_s = (n / 1.0e6) * rtimes / et_s
-    mps_v = (n / 1.0e6) * rtimes / et_v
-
-    return (mps_s, mps_v)
+    return et
 end
+
 
 
 function perf_view(a::Array, title, i1, rtimes::Int)
-    (mps_s, mps_v) = time_view1d(sub(a, i1), view(a, i1), rtimes)
+    et_s = time_view1d(sub(a, i1), rtimes)
+    et_v = time_view1d(view(a, i1), rtimes)
+
+    v = view(a, i1)
+    mps_s = mps(v, rtimes, et_s)
+    mps_v = mps(v, rtimes, et_v)
+
     @printf("%-16s:   %10.3f MPS   %10.3f MPS |  gain = %6.3fx\n", 
         "[$title]", mps_s, mps_v, mps_v / mps_s)
 end
 
 function perf_view(a::Array, title, i1, i2, rtimes::Int)
-    (mps_s, mps_v) = time_view2d(sub(a, i1, i2), view(a, i1, i2), rtimes)
+    et_s = time_view1d(sub(a, i1, i2), rtimes)
+    et_v = time_view1d(view(a, i1, i2), rtimes)
+
+    v = view(a, i1, i2)
+    mps_s = mps(v, rtimes, et_s)
+    mps_v = mps(v, rtimes, et_v)    
+
     @printf("%-16s:   %10.3f MPS   %10.3f MPS |  gain = %6.3fx\n", 
         "[$title]", mps_s, mps_v, mps_v / mps_s)
 end
@@ -80,16 +67,19 @@ end
 
 # benchmarks
 
-const a1 = rand(512)
+println("Indexing              sub              view")
+println("---------------------------------------------------------------------")
 
-perf_view(a1, ":", :, 10^6)
-perf_view(a1, "1:512", 1:512, 10^6)
+const a1 = rand(1024)
+
+perf_view(a1, ":", :, 200000)
+perf_view(a1, "1:1024", 1:1024, 200000)
 
 
-const a2 = rand(32, 16)
+const a2 = rand(32, 32)
 
-perf_view(a2, ":,:", :, :, 200000)
-perf_view(a2, ":,1:32", :, 1:32, 200000)
+perf_view(a2, ":,:", :, :, 100000)
+perf_view(a2, ":,1:32", :, 1:32, 100000)
 
 
 
