9df37e8d9248eb766d8efa7a7a3324ee09e14d9d
Dahua Lin <lindahua@gmail.com>
2014-01-26 11:03:02 -0600

    condensed contiguousview testing

--- test/contiguousview.jl
+++ test/contiguousview.jl
@@ -3,137 +3,74 @@
 using ArrayViews
 using Base.Test
 
-a = rand(5, 4, 3, 2)
+avparent = rand(5, 4, 3, 2)
 
 ## 1D
 
-v = contiguous_view(a, 10, (20,))
+v = contiguous_view(avparent, 10, (20,))
 @test isa(v, ContiguousView{Float64,1})
-
-@test eltype(v) == eltype(a)
+@test eltype(v) == eltype(avparent)
 @test ndims(v) == 1
-
 @test length(v) == 20
 
 @test size(v) == (20,)
-@test size(v,1) == 20
-@test size(v,2) == 1
-
+@test [size(v,i) for i=1:2] == [20, 1]
 @test strides(v) == (1,)
-@test stride(v,1) == 1
-@test stride(v,2) == 20
-@test stride(v,3) == 20
+@test [stride(v,i) for i=1:2] == [1,20]
 
-for i = 1:20
-    @test v[i] == a[10 + i]
-    @test v[i,1] == a[10 + i]
-end
+@test [v[i] for i=1:20] == avparent[11:30]
+@test [v[i,1] for i=1:20] == avparent[11:30]
 
 ## 2D
 
-v = contiguous_view(a, 10, (5, 12))
+v = contiguous_view(avparent, 10, (5, 12))
 @test isa(v, ContiguousView{Float64,2})
-
-@test eltype(v) == eltype(a)
+@test eltype(v) == eltype(avparent)
 @test ndims(v) == 2
-
 @test length(v) == 60
 
 @test size(v) == (5, 12)
-@test size(v,1) == 5
-@test size(v,2) == 12
-@test size(v,3) == 1
-
+@test [size(v,i) for i=1:3] == [5,12,1]
 @test strides(v) == (1,5)
-@test stride(v,1) == 1
-@test stride(v,2) == 5
-@test stride(v,3) == 60
-@test stride(v,4) == 60
+@test [stride(v,i) for i=1:3] == [1,5,60]
 
-for i = 1:60
-    @test v[i] == a[10 + i]
-end
-
-for j = 1:12, i = 1:5
-    @test v[i,j] == a[i,j+2]
-    @test v[i,j,1] == a[i,j+2]
-end
+@test [v[i] for i=1:60] == avparent[11:70]
+@test [v[i,j] for i=1:5, j=1:12] == avparent[:,3:14]
+@test [v[i,j,1] for i=1:5, j=1:12] == avparent[:,3:14]
 
 ## 3D
 
-v = contiguous_view(a, (5, 4, 6))
+v = contiguous_view(avparent, (5, 4, 6))
 @test isa(v, ContiguousView{Float64,3})
-
-@test eltype(v) == eltype(a)
+@test eltype(v) == eltype(avparent)
 @test ndims(v) == 3
-
 @test length(v) == 120
 
 @test size(v) == (5, 4, 6)
-@test size(v,1) == 5
-@test size(v,2) == 4
-@test size(v,3) == 6
-@test size(v,4) == 1
-
+@test [size(v,i) for i=1:4] == [5,4,6,1]
 @test strides(v) == (1,5,20)
-@test stride(v,1) == 1
-@test stride(v,2) == 5
-@test stride(v,3) == 20
-@test stride(v,4) == 120
-@test stride(v,5) == 120
+@test [stride(v,i) for i=1:4] == [1,5,20,120]
 
-for i = 1:120
-    @test v[i] == a[i]
-end
-
-for j = 1:24, i = 1:5
-    @test v[i,j] == a[i,j]
-end
-
-for k = 1:6, j=1:4, i=1:5
-    @test v[i,j,k] == a[i,j,k]
-    @test v[i,j,k,1] == a[i,j,k]
-end
+@test [v[i] for i=1:120] == avparent[1:120]
+@test [v[i,j] for i=1:5, j=1:24] == avparent[1:5,1:24]
+@test [v[i,j,k] for i=1:5, j=1:4, k=1:6] == avparent[1:5, 1:4, 1:6]
+@test [v[i,j,k,1] for i=1:5, j=1:4, k=1:6] == avparent[1:5, 1:4, 1:6]
 
 ## 4D
 
-v = contiguous_view(a, 0, (5, 4, 3, 2))
+v = contiguous_view(avparent, 0, (5, 4, 3, 2))
 @test isa(v, ContiguousView{Float64,4})
-
-@test eltype(v) == eltype(a)
+@test eltype(v) == eltype(avparent)
 @test ndims(v) == 4
-
 @test length(v) == 120
 
 @test size(v) == (5, 4, 3, 2)
-@test size(v,1) == 5
-@test size(v,2) == 4
-@test size(v,3) == 3
-@test size(v,4) == 2
-@test size(v,5) == 1
-
+@test [size(v,i) for i=1:5] == [5,4,3,2,1]
 @test strides(v) == (1,5,20,60)
-@test stride(v,1) == 1
-@test stride(v,2) == 5
-@test stride(v,3) == 20
-@test stride(v,4) == 60
-@test stride(v,5) == 120
-@test stride(v,6) == 120
+@test [stride(v,i) for i=1:5] == [1,5,20,60,120]
 
-for i = 1:120
-    @test v[i] == a[i]
-end
-
-for j = 1:24, i = 1:5
-    @test v[i,j] == a[i,j]
-end
-
-for k = 1:6, j=1:4, i=1:5
-    @test v[i,j,k] == a[i,j,k]
-end
-
-for l = 1:2, k = 1:3, j=1:4, i=1:5
-    @test v[i,j,k,l] == a[i,j,k,l]
-end
-
-
+@test [v[i] for i=1:120] == avparent[1:120]
+@test [v[i,j] for i=1:5, j=1:24] == avparent[1:5, 1:24]
+@test [v[i,j,k] for i=1:5, j=1:4, k=1:6] == avparent[1:5, 1:4, 1:6]
+@test [v[i,j,k,l] for i=1:5, j=1:4, k=1:3, l=1:2] == avparent[1:5, 1:4, 1:3, 1:2]
+@test [v[i,j,k,l,1] for i=1:5, j=1:4, k=1:3, l=1:2] == avparent[1:5, 1:4, 1:3, 1:2]
