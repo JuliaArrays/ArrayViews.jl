591adc35751429e743c86b406173622ef9388838
Dahua Lin <lindahua@gmail.com>
2014-06-10 19:48:24 -0500

    refactor contrank computation

--- src/ArrayViews.jl
+++ src/ArrayViews.jl
@@ -632,20 +632,20 @@ view(a::Array) = contiguous_view(a, size(a))
 view(a::ArrayView) = a
 
 view(a::DenseArray, i::Subs) = 
-    (shp = vshape(a, i); make_view(a, restrict_crank(contrank(a, i), shp), shp, i))
+    (shp = vshape(a, i); make_view(a, restrict_crank(acontrank(a, i), shp), shp, i))
 
 view(a::DenseArray, i1::Subs, i2::Subs) = 
-    (shp = vshape(a, i1, i2); make_view(a, restrict_crank(contrank(a, i1, i2), shp), shp, i1, i2))
+    (shp = vshape(a, i1, i2); make_view(a, restrict_crank(acontrank(a, i1, i2), shp), shp, i1, i2))
 
 view(a::DenseArray, i1::Subs, i2::Subs, i3::Subs) = 
-    (shp = vshape(a, i1, i2, i3); make_view(a, restrict_crank(contrank(a, i1, i2, i3), shp), shp, i1, i2, i3))
+    (shp = vshape(a, i1, i2, i3); make_view(a, restrict_crank(acontrank(a, i1, i2, i3), shp), shp, i1, i2, i3))
 
 view(a::DenseArray, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = 
-    (shp = vshape(a, i1, i2, i3, i4); make_view(a, restrict_crank(contrank(a, i1, i2, i3, i4), shp), shp, i1, i2, i3, i4))
+    (shp = vshape(a, i1, i2, i3, i4); make_view(a, restrict_crank(acontrank(a, i1, i2, i3, i4), shp), shp, i1, i2, i3, i4))
 
 view(a::DenseArray, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
     (shp = vshape(a, i1, i2, i3, i4, i5, I...); 
-     make_view(a, restrict_crank(contrank(a, i1, i2, i3, i4, i5, I...), shp), shp, i1, i2, i3, i4, i5, I...))
+     make_view(a, restrict_crank(acontrank(a, i1, i2, i3, i4, i5, I...), shp), shp, i1, i2, i3, i4, i5, I...))
 
 ## ellipview
 
@@ -685,87 +685,63 @@ restrict_crank{M,N}(::Type{ContRank{M}}, ::NTuple{N,Int}) = ContRank{min(M,N)}
 restrict_crank{N}(::Type{ContRank{0}}, ::NTuple{N,Int}) = ContRank{0}
 restrict_crank{N}(::Type{ContRank{N}}, ::()) = ContRank{0}
 
-# contiguous rank computation based on indices
+### contiguous rank computation based on indices
 
-_nprefixreals() = ContRank{0}
-_nprefixreals(i::Real) = ContRank{1}
-_nprefixreals(i::SubsRange) = ContRank{0}
-
-_nprefixreals(i1::Real, i2::Real) = ContRank{2}
-_nprefixreals(i1::Real, i2::SubsRange) = ContRank{1}
-_nprefixreals(i1::SubsRange, i2::Subs) = ContRank{0}
-
-_nprefixreals(i1::Real, i2::Real, i3::Real) = ContRank{3}
-_nprefixreals(i1::Real, i2::Real, i3::Real, I::Subs...) = 
-    addrank(ContRank{3}, _nprefixreals(I...))
-_nprefixreals(i1::Real, i2::Real, i3::SubsRange, I::Subs...) = ContRank{2}
-_nprefixreals(i1::Real, i2::SubsRange, i3::Subs, I::Subs...) = ContRank{1}
-_nprefixreals(i1::SubsRange, i2::Subs, i3::Subs, I::Subs...) = ContRank{0}
-
+# 0D
 contrank() = ContRank{0}
 
-contrank(i::Real) = ContRank{1}
-contrank(i::Real, i2::Real) = ContRank{2}
-contrank(i::Real, i2::Real, I::Subs...) = _nprefixreals(i, i2, I...)
-contrank(i::Real, i2::SubsRange, I::Subs...) = ContRank{1}
+# 1D
+contrank(i::Union(Colon,UnitRange)) = ContRank{1}
+contrank(i::Subs) = ContRank{0}
 
-contrank(i::Range, I::Subs...) = ContRank{0}
+# 2D
+contrank(i1::Colon, i2::Union(Colon,UnitRange)) = ContRank{2}
+contrank(i1::Colon, i2::Subs) = ContRank{1}
+contrank(i1::UnitRange, i2::Subs) = ContRank{1}
+contrank(i1::Subs, i2::Subs) = ContRank{0}
 
-contrank(i1::Colon) = ContRank{1}
-contrank(i1::Colon, i2::CSubs) = ContRank{2}
-contrank(i1::Colon, i2::Range) = ContRank{1}
+# 3D
+contrank(i1::Colon, i2::Colon, i3::Union(Colon,UnitRange)) = ContRank{3}
+contrank(i1::Colon, i2::Colon, i3::Subs) = ContRank{2}
+contrank(i1::Colon, i2::UnitRange, i3::Subs) = ContRank{2}
+contrank(i1::Colon, i2::Subs, i3::Subs) = ContRank{1}
+contrank(i1::UnitRange, i2::Subs, i3::Subs) = ContRank{1}
+contrank(i1::Subs, i2::Subs, i3::Subs) = ContRank{0}
 
-contrank(i1::Colon, i2::Colon, i3::Real) = ContRank{3}
-contrank(i1::Colon, i2::Colon, i3::CSubsRange) = ContRank{3}
-contrank(i1::Colon, i2::Colon, i3::Range) = ContRank{2}
-contrank(i1::Colon, i2::Colon, i3::Colon, i4::Real) = ContRank{4}
-contrank(i1::Colon, i2::Colon, i3::Colon, i4::CSubsRange) = ContRank{4}
-contrank(i1::Colon, i2::Colon, i3::Colon, i4::Range) = ContRank{3}
-
-contrank(i1::Colon, i2::Colon, i3::Colon, i4::Colon) = ContRank{4}
-contrank(i1::Colon, i2::Colon, i3::Colon, i4::Colon, I::Subs...) = 
-    addrank(ContRank{4}, contrank(I...))
-contrank(i1::Colon, i2::Colon, i3::Real, I::Subs...) = 
-    addrank(ContRank{3}, _nprefixreals(I...))
-contrank(i1::Colon, i2::Colon, i3::Range1, I::Subs...) = 
-    addrank(ContRank{3}, _nprefixreals(I...))
-contrank(i1::Colon, i2::Colon, i3::Range,  I::Subs...) = ContRank{2}
-
-contrank(i1::Colon, i2::Union(Real,Range1), I::Subs...) = addrank(ContRank{2}, _nprefixreals(I...))
-contrank(i1::Colon, i2::Range, I::Subs...) = ContRank{1}
-
-contrank(i1::Range1) = ContRank{1}
-contrank(i1::Range1, i2::Real) = ContRank{2}
-contrank(i1::Range1, i2::SubsRange, I::Subs...) = ContRank{1}
-
-contrank(i1::Range1, i2::Real, i3::Real) = ContRank{3}
-contrank(i1::Range1, i2::Real, I::Subs...) = addrank(ContRank{2}, _nprefixreals(I...))
+# ND
+contrank(i1::Colon, i2::Colon, i3::Colon, I::Subs...) = addrank(ContRank{3}, contrank(I...))
+contrank(i1::Colon, i2::Colon, i3::UnitRange, I::Subs...) = ContRank{3}
+contrank(i1::Colon, i2::Colon, i3::Subs, I::Subs...) = ContRank{2}
+contrank(i1::Colon, i2::UnitRange, i3::Subs, I::Subs...) = ContRank{2}
+contrank(i1::Colon, i2::Subs, i3::Subs, I::Subs...) = ContRank{1}
+contrank(i1::UnitRange, i2::Subs, i3::Subs, I::Subs...) = ContRank{1}
+contrank(i1::Subs, i2::Subs, i3::Subs, I::Subs...) = ContRank{0}
 
 # contiguous rank with array & arrayviews
 
-contrank(a::Array, i1::Subs) = contrank(i1)
-contrank(a::Array, i1::Subs, i2::Subs) = contrank(i1, i2)
-contrank(a::Array, i1::Subs, i2::Subs, i3::Subs) = contrank(i1, i2, i3)
-contrank(a::Array, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = contrank(i1, i2, i3, i4)
-contrank(a::Array, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
+acontrank(a::Array, i1::Subs) = contrank(i1)
+acontrank(a::Array, i1::Subs, i2::Subs) = contrank(i1, i2)
+acontrank(a::Array, i1::Subs, i2::Subs, i3::Subs) = contrank(i1, i2, i3)
+acontrank(a::Array, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = contrank(i1, i2, i3, i4)
+acontrank(a::Array, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
     contrank(i1, i2, i3, i4, i5, I...)
 
-contrank{T,N}(a::ArrayView{T,N,N}, i1::Subs) = contrank(i1)
-contrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs) = contrank(i1, i2)
-contrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs) = contrank(i1, i2, i3)
-contrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = 
+acontrank{T,N}(a::ArrayView{T,N,N}, i1::Subs) = contrank(i1)
+acontrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs) = contrank(i1, i2)
+acontrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs) = contrank(i1, i2, i3)
+acontrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = 
     contrank(i1, i2, i3, i4)
-contrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
+acontrank{T,N}(a::ArrayView{T,N,N}, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
     contrank(i1, i2, i3, i4, i5, I...)
 
-contrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs) = minrank(contrank(i1), ContRank{M})
-contrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs) = 
+acontrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs) = minrank(contrank(i1), ContRank{M})
+acontrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs) = 
     minrank(contrank(i1, i2), ContRank{M})
-contrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs) = 
+acontrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs) = 
     minrank(contrank(i1, i2, i3), ContRank{M})
-contrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = 
+acontrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs, i4::Subs) = 
     minrank(contrank(i1, i2, i3, i4), ContRank{M})
-contrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
+acontrank{T,N,M}(a::ArrayView{T,N,M}, i1::Subs, i2::Subs, i3::Subs, i4::Subs, i5::Subs, I::Subs...) = 
     minrank(contrank(i1, i2, i3, i4, i5, I...), ContRank{M})
 
 end  # module ArrayViews
--- test/contrank.jl
+++ test/contrank.jl
@@ -9,146 +9,60 @@ const icolon = (:)
 const irange = 1:5
 const istepr = 1:2:5
 
+const tinds = {irealn, icolon, irange, istepr}
+
 crank{M}(::Type{ContRank{M}}) = M
 crank(I::Subs...) = crank(contrank(I...))
 
+function safe_crank(inds...)
+    r = 0
+    for i in inds
+        if isa(i, Colon)
+            r += 1
+        elseif isa(i, UnitRange)
+            r += 1
+            return r
+        else
+            return r
+        end
+    end
+    return r
+end
 
-@test crank() == 0
+function test_crank(inds...)
+    c = crank(inds...)
+    s = safe_crank(inds...)
+    if c != s
+        error("Incorrect contrank result, on $(inds) ==> crank = $(c), safe_crank = $(s)")
+    end
+end
+
+# 0D
+test_crank()
 
 # 1D
-
-@test crank(irealn) == 1
-@test crank(icolon) == 1
-@test crank(irange) == 1
-@test crank(istepr) == 0
+for i1 in tinds
+    test_crank(i1)
+end
 
 # 2D
-
-@test crank(irealn, irealn) == 2
-@test crank(irealn, icolon) == 1
-@test crank(irealn, irange) == 1
-@test crank(irealn, istepr) == 1
-
-@test crank(icolon, irealn) == 2
-@test crank(icolon, icolon) == 2
-@test crank(icolon, irange) == 2
-@test crank(icolon, istepr) == 1
-
-@test crank(irange, irealn) == 2
-@test crank(irange, icolon) == 1
-@test crank(irange, irange) == 1
-@test crank(irange, istepr) == 1
-
-@test crank(istepr, irealn) == 0
-@test crank(istepr, icolon) == 0
-@test crank(istepr, irange) == 0
-@test crank(istepr, istepr) == 0
+for i1 in tinds, i2 in tinds
+    test_crank(i1, i2)
+end
 
 # 3D
+for i1 in tinds, i2 in tinds, i3 in tinds
+    test_crank(i1, i2, i3)
+end
 
-@test crank(irealn, irealn, irealn) == 3
-@test crank(irealn, irealn, icolon) == 2
-@test crank(irealn, irealn, irange) == 2
-@test crank(irealn, irealn, istepr) == 2
+# 4D
+for i1 in tinds, i2 in tinds, i3 in tinds, i4 in tinds
+    test_crank(i1, i2, i3, i4)
+end
 
-@test crank(irealn, icolon, irealn) == 1
-@test crank(irealn, icolon, icolon) == 1
-@test crank(irealn, icolon, irange) == 1
-@test crank(irealn, icolon, istepr) == 1
+# 5D
+for i1 in tinds, i2 in tinds, i3 in tinds, i4 in tinds, i5 in tinds
+    test_crank(i1, i2, i3, i4, i5)
+end
 
-@test crank(irealn, irange, irealn) == 1
-@test crank(irealn, irange, icolon) == 1
-@test crank(irealn, irange, irange) == 1
-@test crank(irealn, irange, istepr) == 1
-
-@test crank(irealn, istepr, irealn) == 1
-@test crank(irealn, istepr, icolon) == 1
-@test crank(irealn, istepr, irange) == 1
-@test crank(irealn, istepr, istepr) == 1
-
-@test crank(icolon, irealn, irealn) == 3
-@test crank(icolon, irealn, icolon) == 2
-@test crank(icolon, irealn, irange) == 2
-@test crank(icolon, irealn, istepr) == 2
-
-@test crank(icolon, icolon, irealn) == 3
-@test crank(icolon, icolon, icolon) == 3
-@test crank(icolon, icolon, irange) == 3
-@test crank(icolon, icolon, istepr) == 2
-
-@test crank(icolon, irange, irealn) == 3
-@test crank(icolon, irange, icolon) == 2
-@test crank(icolon, irange, irange) == 2
-@test crank(icolon, irange, istepr) == 2
-
-@test crank(icolon, istepr, irealn) == 1
-@test crank(icolon, istepr, icolon) == 1
-@test crank(icolon, istepr, irange) == 1
-@test crank(icolon, istepr, istepr) == 1
-
-@test crank(irange, irealn, irealn) == 3
-@test crank(irange, irealn, icolon) == 2
-@test crank(irange, irealn, irange) == 2
-@test crank(irange, irealn, istepr) == 2
-
-@test crank(irange, icolon, irealn) == 1
-@test crank(irange, icolon, icolon) == 1
-@test crank(irange, icolon, irange) == 1
-@test crank(irange, icolon, istepr) == 1
-
-@test crank(irange, irange, irealn) == 1
-@test crank(irange, irange, icolon) == 1
-@test crank(irange, irange, irange) == 1
-@test crank(irange, irange, istepr) == 1
-
-@test crank(irange, istepr, irealn) == 1
-@test crank(irange, istepr, icolon) == 1
-@test crank(irange, istepr, irange) == 1
-@test crank(irange, istepr, istepr) == 1
-
-@test crank(istepr, irealn, irealn) == 0
-@test crank(istepr, irealn, icolon) == 0
-@test crank(istepr, irealn, irange) == 0
-@test crank(istepr, irealn, istepr) == 0
-
-@test crank(istepr, icolon, irealn) == 0
-@test crank(istepr, icolon, icolon) == 0
-@test crank(istepr, icolon, irange) == 0
-@test crank(istepr, icolon, istepr) == 0
-
-@test crank(istepr, irange, irealn) == 0
-@test crank(istepr, irange, icolon) == 0
-@test crank(istepr, irange, irange) == 0
-@test crank(istepr, irange, istepr) == 0
-
-@test crank(istepr, istepr, irealn) == 0
-@test crank(istepr, istepr, icolon) == 0
-@test crank(istepr, istepr, irange) == 0
-@test crank(istepr, istepr, istepr) == 0
-
-# Some 4D
-
-@test crank(icolon, icolon, irealn, irealn) == 4
-@test crank(icolon, icolon, irealn, icolon) == 3
-@test crank(icolon, icolon, irealn, irange) == 3
-@test crank(icolon, icolon, irealn, istepr) == 3
-
-@test crank(icolon, icolon, icolon, irealn) == 4
-@test crank(icolon, icolon, icolon, icolon) == 4
-@test crank(icolon, icolon, icolon, irange) == 4
-@test crank(icolon, icolon, icolon, istepr) == 3
-
-@test crank(icolon, icolon, irange, irealn) == 4
-@test crank(icolon, icolon, irange, icolon) == 3
-@test crank(icolon, icolon, irange, irange) == 3
-@test crank(icolon, icolon, irange, istepr) == 3
-
-@test crank(icolon, icolon, istepr, irealn) == 2
-@test crank(icolon, icolon, istepr, icolon) == 2
-@test crank(icolon, icolon, istepr, irange) == 2
-@test crank(icolon, icolon, istepr, istepr) == 2
-
-# Some 5D
-
-@test crank(icolon, icolon, icolon, icolon, irealn) == 5
 
