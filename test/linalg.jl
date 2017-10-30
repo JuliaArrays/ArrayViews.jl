d32696431d85648cc5885440a6b45b8cdf23a334
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2017-10-30 18:47:59 -0400

    Fix a Test deprecation

--- test/linalg.jl
+++ test/linalg.jl
@@ -11,7 +11,7 @@ _ab = avparent * bvparent
 @test _ab == aview(avparent, :, :) * aview(bvparent, :, :)
 
 for j = 1:size(bvparent,2)
-    @test_approx_eq _ab[:,j] aview(avparent,:,:) * aview(bvparent,:,j)
+    @test _ab[:,j] ≈ aview(avparent,:,:) * aview(bvparent,:,j)
 end
 
 @test avparent[:, 2:2:7] * bvparent[1:3, 1:2:5] == aview(avparent, :, 2:2:7) * aview(bvparent, 1:3, 1:2:5)
